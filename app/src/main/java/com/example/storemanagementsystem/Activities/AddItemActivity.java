3c77f0e97150d57255b93e906ebe4903fd1eace2
kristijanzrno <superkiki.info.android@gmail.com>
2020-01-24 10:23:12 +0000

    Initial commit

---
+++ app/src/main/java/com/example/storemanagementsystem/Activities/AddItemActivity.java
@@ -0,0 +1,16 @@
+package com.example.storemanagementsystem.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.storemanagementsystem.R;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item_manually);
+    }
+}
---
+++ app/src/main/java/com/example/storemanagementsystem/Activities/BasketActivity.java
@@ -0,0 +1,16 @@
+package com.example.storemanagementsystem.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.storemanagementsystem.R;
+
+public class BasketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+    }
+}

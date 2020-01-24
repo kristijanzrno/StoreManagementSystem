1d5d3ed5316145ca198250e832b3450eb48ebdba
kristijanzrno <superkiki.info.android@gmail.com>
2020-01-24 11:03:46 +0000

    LoginScreen designed, started designing basket item

--- app/src/main/java/com/example/storemanagementsystem/Activities/BasketActivity.java
+++ app/src/main/java/com/example/storemanagementsystem/Activities/BasketActivity.java
@@ -1,7 +1,9 @@
 package com.example.storemanagementsystem.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.storemanagementsystem.R;
@@ -12,5 +14,15 @@ public class BasketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+
+        Intent i = new Intent(this, ItemScannerActivity.class);
+        startActivity(i);
+    }
+
+
+    // Results from the scanner
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/example/storemanagementsystem/Activities/LoginActivity.java
+++ app/src/main/java/com/example/storemanagementsystem/Activities/LoginActivity.java
@@ -2,15 +2,34 @@ package com.example.storemanagementsystem.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.storemanagementsystem.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
 
+    @BindView(R.id.usernameTB)
+    EditText usernameTB;
+    @BindView(R.id.passwordTB)
+    EditText passwordTB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.loginButton)
+    public void login(View v){
+        // If authentication == true
+        Intent i = new Intent(LoginActivity.this, BasketActivity.class);
+        startActivity(i);
     }
 }

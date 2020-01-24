f68581296ffb0537ed3be8693749cd2f4380c652
kristijanzrno <superkiki.info.android@gmail.com>
2020-01-24 12:19:46 +0000

    Designed basket activity and added main functionality

---
+++ app/src/main/java/Adapters/BasketRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.storemanagementsystem.R;
+
+
+import java.util.ArrayList;
+
+import Data.PurchaseItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BasketRecyclerViewAdapter extends RecyclerView.Adapter<BasketRecyclerViewAdapter.ViewHolder> {
+    private ArrayList<PurchaseItem> items;
+
+    public BasketRecyclerViewAdapter(@NonNull ArrayList<PurchaseItem> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public BasketRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.basket_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BasketRecyclerViewAdapter.ViewHolder holder, int position) {
+        holder.itemName.setText(items.get(position).getItemName());
+        holder.quantityTV.setText(""+items.get(position).getQuantity());
+
+        holder.increaseQuantity.setOnClickListener(v -> {
+            items.get(position).increaseQuantity();
+            notifyDataSetChanged();
+        });
+
+        holder.decreaseQuantity.setOnClickListener(v -> {
+            items.get(position).decreaseQuantity();
+            notifyDataSetChanged();
+        });
+        holder.removeItem.setOnClickListener(v -> {
+            items.remove(position);
+            notifyDataSetChanged();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.basketItemName)
+        TextView itemName;
+        @BindView(R.id.quantityTV)
+        TextView quantityTV;
+        @BindView(R.id.basketIncreaseQuantity)
+        CardView increaseQuantity;
+        @BindView(R.id.basketDecreaseQuantity)
+        CardView decreaseQuantity;
+        @BindView(R.id.basketRemoveItem)
+        ImageView removeItem;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/com/example/storemanagementsystem/Activities/BasketActivity.java
+++ app/src/main/java/com/example/storemanagementsystem/Activities/BasketActivity.java
@@ -2,21 +2,78 @@ package com.example.storemanagementsystem.Activities;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Toast;
 
 import com.example.storemanagementsystem.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class BasketActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import Adapters.BasketRecyclerViewAdapter;
+import Data.PurchaseItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class BasketActivity extends AppCompatActivity{
+
+
+    @BindView(R.id.basketRecyclerView)
+    RecyclerView recyclerView;
+
+    private ArrayList<PurchaseItem> items = new ArrayList<>();
+    BasketRecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+        ButterKnife.bind(this);
+        addTestData();
+        setupViews();
+    }
 
-        Intent i = new Intent(this, ItemScannerActivity.class);
-        startActivity(i);
+
+    private void addTestData(){
+        for(int i = 0; i<3; i++){
+            PurchaseItem item = new PurchaseItem();
+            item.setId("id");
+            item.setItemName("Test item " + i);
+            item.setQuantity(i);
+            items.add(item);
+        }
+    }
+
+    private void setupViews(){
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        adapter = new BasketRecyclerViewAdapter(items);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+
+    @OnClick(R.id.scanItem)
+    public void scanItem(View view){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setPrompt("Please scan an item QR Code...");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+    }
+
+    @OnClick(R.id.confirmPurchase)
+    public void confirmPurchase(View view){
+        Toast.makeText(this, "Confirm purchase TODO...", Toast.LENGTH_SHORT).show();
     }
 
 
@@ -24,5 +81,20 @@ public class BasketActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if(result != null && result.getContents() != null){
+                PurchaseItem item = new PurchaseItem();
+                item.setId("id");
+                item.setItemName("Scanned item n");
+                item.setQuantity(1);
+                items.add(item);
+                Toast.makeText(this, "Successfully scanned item", Toast.LENGTH_SHORT).show();
+                adapter.notifyDataSetChanged();
+            }
+        }
     }
+
+
 }
+
